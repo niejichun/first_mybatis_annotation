da382ecfd853b7295554837eb62841522f9ebbd7
niejichun <jichun.nie@missionpublic.com>
2019-05-25 23:24:11 +0800

    transform project

--- src/main/java/com/itheima/dao/IUserDao.java
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -1,7 +1,10 @@
 package com.itheima.dao;
 
 import com.itheima.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -9,4 +12,14 @@ import java.util.List;
 public interface IUserDao {
     @Select("select * from user")
     List<User> findAll();
+
+    @Update("update user set username=#{username},birthday=#{birthday},sex=#{sex},address=#{address} where id=#{id}")
+    void updateUser(User user);
+
+    @Insert("insert user (username,birthday,sex,address) values (#{username},#{birthday},#{sex},#{address})")
+    void insertUser(User user);
+
+    @Delete("delete from user where id=#{userId}")
+    void deleteUser(Integer userId);
+
 }
--- src/test/java/com/itheima/test/MybatisTest.java
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -6,30 +6,77 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class MybatisTest {
-    public static void main(String[] args) throws Exception {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before
+    public void init() throws Exception {
         //1 读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //2 创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //3 使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
-        //4 使用SqlSession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        // 5 使用代理对象执行方法
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2 创建SqlSessionFactory工厂，创建者模式
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3 使用工厂生产SqlSession对象  工厂模式
+        sqlSession = factory.openSession();
+        //4 使用SqlSession创建Dao接口的代理对象    代理模式
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After
+    public void destroy() throws Exception {
+        //插入时必须加上提交事务，否则不生效
+        sqlSession.commit();
+        //释放资源
+        sqlSession.close();
+        in.close();
+    }
+
+    //---------------------------select-----------------------------------------------------
+    //all
+    @Test
+    public void testFindAll() {
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
-        //6 释放资源
-        session.close();
-        in.close();
+    }
 
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setId(1);
+        user.setAddress("12121221");
+        user.setBirthday(new Date());
+        user.setSex("男");
+        user.setUsername("王121五");
+
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setAddress("456456");
+        user.setBirthday(new Date());
+        user.setSex("男");
+        user.setUsername("王asd五");
+
+        userDao.insertUser(user);
+//        System.out.println("新增记录之后的主键ID" + user.getId());
+    }
+
+    @Test
+    public void deleteUser() {
+        userDao.deleteUser(1);
     }
 
 }
